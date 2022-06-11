2d9fb662b27ad4103cd9e52e32ec2d8845d2e0da
ofdove <ofdove528@outlook.com>
2022-06-11 11:31:39 +0800

    iperfmulti added

--- mininet/cli.py
+++ mininet/cli.py
@@ -156,6 +156,21 @@ class CLI( Cmd ):
         if line == '':
             output( self.helpStr )
 
+    def do_iperfmulti( self, line):
+        """Multi iperf UDP test between nodes"""
+        args = line.split()
+        if len(args) == 1:
+            udpBw = args[0]
+            self.mn.iperfMulti(udpBw)
+        elif len(args) == 2:
+            udpBw = args[0]
+            period = args[1]
+            err = False
+            self.mn.iperfMulti(udpBw, float(period))
+        else:
+            error('invalid number of args: iperfmulti udpBw \n' + 
+                    'udpBw examples: 1M\n')
+
     def do_nodes( self, _line ):
         "List all nodes."
         nodes = ' '.join( sorted( self.mn ) )
--- mininet/net.py
+++ mininet/net.py
@@ -848,6 +848,49 @@ class Mininet( object ):
         output( '*** Results: %s\n' % result )
         return result
 
+    def iperf_single(self, hosts=None, udpBw='10M', period=5, port=5001):
+        """Run iperf between two hosts using UDP.
+           hosts: list of hosts; if None, uses opposite hosts
+           returns: results two-element array of server and client speeds"""
+        if not hosts:
+            return
+        else:
+            assert len(hosts) == 2
+        client, server = hosts
+        filename = client.name[1:] + '.out'
+        output('*** Iperf: testing bandwith between ')
+        output("%s and %s\n" % (client.name, server.name))
+        iperfArgs = 'iperf -u '
+        bwArgs = '-b ' + udpBw + ' '
+        print "***start server***"
+        server.cmd(iperfArgs + '-s' + ' > $HOME/log/' + filename + '&')
+        print "***start client***"
+        client.cmd(
+                iperfArgs + '-t ' + str(period) + ' -c ' + server.IP() + ' '  
+                + bwArgs + ' > $HOME/log/' + 'client' + filename + '&'
+                )
+    def iperfMulti(self, bw, period=5):
+        base_port = 5001
+        server_list = []
+        client_list = [h for h in self.hosts]
+        host_list = []
+
+        cli_outs = []
+        ser_outs = []
+
+        _len = len(host_list)
+        for i in xrange(0, _len):
+            client = host_list[i]
+            server = client
+            while(server == client):
+                server = random.choice(host_list)
+                server_list.append(server)
+                self.iperf_single(hosts = [client, server], udpBw=bw, period=period, port=base_port)
+                sleep(.05)
+                base_port += 1
+            self.hosts[0].cmd('ping -c10' + self.hosts[-1].IP() + ' > $HOME/log/delay.out')
+            sleep(period)
+
     def runCpuLimitTest( self, cpu, duration=5 ):
         """run CPU limit test with 'while true' processes.
         cpu: desired CPU fraction of each host
